d58cf5b5bef3f71ace7ca74ed6aa4e6c03e7a801
gdodge0 <gabriel@gdodge.dev>
2023-10-27 16:44:28 -0700

    Custom user model

--- editor/models.py
+++ editor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from main.models import User
+
 
 # Create your models here.
+
+class Document(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Document Owner
+
+
+
--- pl/settings.py
+++ pl/settings.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+AUTH_USER_MODEL = "main.User"
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
