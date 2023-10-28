2dcebf48273e19d3ce003e2e35dcba8d190c09ab
Sterling Radisay <Sterlingradisay@gmail.com>
2023-10-27 19:02:32 -0700

    Added some views

--- editor/views.py
+++ editor/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import RootDocument
+
 
 # Create your views here.
+
+def document_view(request, document_id):
+    document = RootDocument.objects.get_object_or_404(id=document_id)
+    return render(request, 'editor/editor.html', {"doc": document})
+
+
+
