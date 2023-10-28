adb248511248bf561a5a7024df0c904db70b6f5f
Sterling Radisay <Sterlingradisay@gmail.com>
2023-10-28 13:59:18 -0700

    Added dashboard preview frame, no backend

--- static/js/dashboard/dashboard.js
+++ static/js/dashboard/dashboard.js
@@ -4,7 +4,23 @@ $(document).ready(function() {
     docs.on("click", function() {
         $(".document").removeClass("document-active");
         $(this).addClass("document-active");
+        let text = $(this).find("span").first().text();
+        $("#document-name").text(text);
+        $("#pages").removeClass("d-none");
 
+        fetch("/api/get_document?id="+$(this).data("id"), {
+            method: "GET",
+        }).then(response => response.json())
+        .then(data => {
+
+
+        });
+
+    });
+
+    $("#go-btn").on("click", function() {
+        let id = $(".document-active").data("id"); // Get the link URL
+        window.open("/editor/"+id, '_blank');
     });
 
     docs.on("dblclick", function()
