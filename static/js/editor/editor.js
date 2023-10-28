2481eed105c428add738a0ddf42db32060bb3128
Evan Shahrestany <eshahrestany@rapidcovidlabs.com>
2023-10-28 12:16:34 -0700

    updates to editor frontend

--- static/js/editor/editor.js
+++ static/js/editor/editor.js
@@ -1,27 +1,64 @@
-// JS for editor frontend
+// Scripts for editor frontend
 
 const editor = document.getElementById("editor");
 
 // start with editor focused
 let editor_focused = true;
-document.addEventListener("keydown", logKeyPressed);
-
 editor.addEventListener('focus', () => { editor_focused = true });
 editor.addEventListener('blur', () => { editor_focused = false });
 
 
-function logKeyPressed(event) {
+document.addEventListener("keydown", (event) => {
     if (editor_focused) {
-        // Get the key code or key from the event object
-        const keyPressed = event.key || String.fromCharCode(event.keyCode);
+        // Get the key from the event object
+        const key = event.key
 
-        // Log the key
-        console.log("Key pressed: " + keyPressed);
+        // Check for toolbar actions
+        // TODO: handle more toolbar shortcut actions
+        if (event.ctrlKey) {
+            if (key.toLowerCase() === "b") {
+                console.log("bolden B)");
+            }
+        }
+        else if (key.length === 1 || key === "Backspace" || key === "Delete") {
+            let pos;
+            const selection = window.getSelection();
+            if (selection.isCollapsed){
+                const range = selection.getRangeAt(0);
+                let parentElement = range.startContainer.parentNode;
+                 pos = {
+                    selection: false,
+                    base_delta: parentElement.id,
+                    index: range.startOffset
+                }
+            }
+            else {
+                const range = selection.getRangeAt(0);
+                const startNode = range.startContainer.parentNode;
+                const endNode = range.endContainer.parentNode;
+                const startIndex = range.startOffset;
+                const endIndex = range.endOffset;
+                pos = {
+                    selection: true,
+                    start_delta: startNode.id,
+                    start_index: startIndex,
+                    end_delta: endNode.id,
+                    end_index: endIndex
+                }
+            }
+            update(key, pos);
+        }
     }
-}
+});
+
+document.addEventListener('paste', (event) => {
+    const clipboardData = event.clipboardData;
+
+});
 
 
-// rewrite deprecated text modifiers below later
+
+
 function changeColor() {
     const color = prompt("Enter your color in hex ex:#f1f233");
     document.execCommand("foreColor", false, color);
