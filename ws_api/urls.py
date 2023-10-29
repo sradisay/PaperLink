3817147874f98a8e48531093c20b25ea2926147a
gdodge0 <gabriel@gdodge.dev>
2023-10-29 00:59:42 -0700

    remove is root

--- ws_api/urls.py
+++ ws_api/urls.py
@@ -3,7 +3,7 @@ from . import views
 from ws_api import consumers
 
 ws_urlpatterns = [
-    re_path(r"ws/edit/(?P<doc_pk>\w+)-(?P<is_root>\w+)/$", consumers.EditConsumer.as_asgi())
+    re_path(r"ws/edit/(?P<doc_pk>\w+)/$", consumers.EditConsumer.as_asgi())
 ]
 
 urlpatterns = [
