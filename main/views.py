6e5ef3d8f7ad382eb66f82208b059a71229c02c9
pyuku <aren.jpg@gmail.com>
2023-10-29 00:20:07 -0700

    :3

--- main/views.py
+++ main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-
+from django.contrib.auth import logout
 
 # Create your views here.
 def homepage(request):
     return render(request, template_name="main/home.html")
-
 
 # Create your views here.
 def register(request):
@@ -24,6 +23,10 @@ def register(request):
 
 def info(request):
     return render(request, 'info/info.html')
+
+
+
+
 
 
 
